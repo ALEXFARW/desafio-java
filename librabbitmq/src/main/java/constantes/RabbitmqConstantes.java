81ad3eae5c6e73dab75eca41dfeffa0d354378e3
ALEXFARW <alexfarw@gmail.com>
2023-12-26 20:56:52 -0300

    Add files via upload
    Esse library concentra alguns recursos comuns a serem usados pelos serviços de geração de pedidos/produtos, producer e consumer de mensagens rabbitMQ

---
+++ librabbitmq/src/main/java/constantes/RabbitmqConstantes.java
@@ -0,0 +1,11 @@
+package constantes;
+
+/**
+ *
+ */
+public class RabbitmqConstantes {
+  public static final String FILA_PEDIDOS   = "PEDIDOS";
+  public static final String FILA_ESTOQUE = "ESTOQUE";
+  public static final String FILA_PRECO   = "PRECO";
+
+}
